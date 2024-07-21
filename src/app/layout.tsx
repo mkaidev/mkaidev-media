e367b427a36e4ffac518385cc12b7658af8a050f
mkaidev <mkaidev88@gmail.com>
2024-07-22 03:30:59 +0700

    creating post-tiptap editor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 
 import localFont from "next/font/local";
+import { Toaster } from "@/components/ui/toaster";
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -37,6 +39,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
---
+++ src/components/posts/editor/PostEditor.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
+import { EditorContent, useEditor } from "@tiptap/react";
+
+import { useSession } from "@/app/(main)/SessionProvider";
+import { submitPost } from "./actions";
+import "./styles.css";
+import UserAvatar from "@/components/UserAvatar";
+import { Button } from "@/components/ui/button";
+
+export default function PostEditor() {
+  const { user } = useSession();
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        bold: false,
+        italic: false,
+      }),
+      Placeholder.configure({
+        placeholder: "What's crack-a-lackin'?",
+      }),
+    ],
+  });
+
+  const input =
+    editor?.getText({
+      blockSeparator: "\n",
+    }) || "";
+
+  async function onSubmit() {
+    await submitPost(input);
+    editor?.commands.clearContent();
+  }
+
+  return (
+    <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex gap-5">
+        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
+        <EditorContent
+          editor={editor}
+          className="max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3"
+        />
+      </div>
+      <div className="flex justify-end">
+        <Button
+          onClick={onSubmit}
+          disabled={!input.trim()}
+          className="min-w-20"
+        >
+          Post
+        </Button>
+      </div>
+    </div>
+  );
+}
